f0a80677be6daa754c7a37de8cee89ee976a79b8
madan <madan.pandiri@artesyn.com>
2015-08-06 18:38:50 +0530

    Line segment intersection 	new file:   LSI.java 	new file:   Point.java

---
+++ Princeton/algs-1/week5/Point.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Point implements Comparable <Point>{
+        final private int x;
+        final private int y;
+        private char pos;
+        public Point(int X, int Y, char POS)
+        {
+            x = X;
+            y = Y;
+            pos = POS;
+        }
+
+        public int compareTo(Point that)
+        {
+            if (this.x < that.y) return -1;
+            if (this.x > that.y) return +1;
+            if (this.y < that.y) return -1;
+            if (this.y > that.y) return +1;
+            return 0;
+        }
+        public char pos()
+        {
+            return pos;
+        }
+        public int x()
+        {
+            return x;
+        }
+        public int y()
+        {
+            return y;
+        }
+    }
+
+
